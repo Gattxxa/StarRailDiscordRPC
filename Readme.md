c3ed86ffab25f1e02a07c792d2696db0cbb6c628
Gattxxa <mail@gattxxa.org>
2023-05-05 18:59:10 +0900

    Rules are Meant to Be Broken!

---
+++ Readme.md
@@ -0,0 +1,34 @@
+# <img src="https://download.gattxxa.org/StarRailDiscordRPC/favicon.ico" align="top" style="width: 1.4em"> StarRailDiscordRPC.exe  
+**[崩壊：スターレイル](https://hsr.hoyoverse.com/)を起動している間、DiscordのアクティビティにRich Presenceで表示させるやつ。**  
+仕様的に日本ユーザー向けです。
+  
+![](https://download.gattxxa.org/StarRailDiscordRPC/screenshot.png)
+
+## 使い方
+### 概要
+崩壊：スターレイルを起動している間、DiscordのアクティビティにRich Presence として反映します。  
+Windowsの言語設定が日本語以外の場合、正常な動作を保証できません。  
+起動後はタスクトレイに常駐します。右クリックで出現するメニューから終了させられます。  
+このアプリケーションは多重起動できますが推奨しません。  
+  
+### パソコン起動時に自動で起動させる
+1\. ` StarRailDiscordRPC.exe ` を分かりやすい場所に配置。（ドキュメントなど）  
+2\. ` StarRailDiscordRPC.exe ` のショートカットを作成する。  
+3\. [Win] + [R] の同時押しで開いたウィンドウに ` shell:startup ` と入力して OK を押す。  
+4\. 開かれたフォルダに ` StarRailDiscordRPC.exe - ショートカット ` を配置する。  
+
+## ダウンロード
+セキュリティソフトに検知される場合がありますので、信頼できる方のみ利用してください。  
+
+| Version  | Time | Download |
+| - | - | - |
+| **latest ☆**  | **undefined**  | **[こちら](https://github.com/Gattxxa/StarRailDiscordRPC/releases)** |
+| 1.0.3  | 2023-05-05  | 配布終了 |
+| 1.0.2  | 2023-05-03  | 配布終了 |
+| 1.0.1  | 2023-04-30  | 配布終了 |
+| 1.0.0 | 2023-04-29 | 配布終了 |
+
+## スペシャルサンクス
+- [niveshbirangal / discord-rpc](https://github.com/niveshbirangal/discord-rpc)  
+- [【Python編】DiscordRPCを使ってプロフィールに乗せるまで](https://qiita.com/taitaitatata/items/1bcec7c09424518fb2af)
+- [Windows のタスクトレイに Python アプリを常駐させ定期的にプログラムを実行する](https://qiita.com/bassan/items/3025eeb6fd2afa03081b)  
---
+++ src/main.py
@@ -0,0 +1,108 @@
+import os
+from pystray import Icon, MenuItem, Menu
+from PIL import Image
+import rpc
+import schedule
+import sys
+import threading
+import time
+import webbrowser
+import win32gui
+
+
+WINDOW_TITLE: str = "崩壊：スターレイル"
+CLIENT_ID: str = "1101576946086838365"
+VERSION: str = "1.1.0"
+WEBSITE: str = "https://download.gattxxa.org/StarRailDiscordRPC/"
+
+cooldown: int = 0
+now_playing: bool = False
+status_updated: bool = False
+timestamp = time.mktime(time.localtime())
+client: rpc.WinDiscordIpcClient = rpc.DiscordIpcClient
+
+
+def resource_path(relative_path):
+    if hasattr(sys, "_MEIPASS"):
+        return os.path.join(sys._MEIPASS, relative_path)
+    return os.path.join(os.path.abspath("."), relative_path)
+
+
+def find_window(hwnd, window_title):
+    global now_playing
+
+    if window_title in win32gui.GetWindowText(hwnd):
+        now_playing = True
+        return hwnd
+
+
+def update_status():
+    global cooldown, timestamp, now_playing, status_updated, client
+
+    try: win32gui.EnumWindows(find_window, WINDOW_TITLE)
+    except: return
+        
+    if now_playing and status_updated:
+        cooldown -= 1
+    
+    elif now_playing:
+        if not status_updated:
+            status_updated = True
+            timestamp = time.mktime(time.localtime())
+        if cooldown < 1:
+            cooldown = 300
+            client = rpc.DiscordIpcClient.for_platform(CLIENT_ID)
+            activity = {
+                "timestamps": {
+                    "start": timestamp
+                },
+                "assets": {
+                    "large_image": "application",
+                    "large_text": "崩壊：スターレイル"
+                }
+            }
+            client.set_activity(activity)
+                    
+    elif status_updated:
+        status_updated = False
+        cooldown = 0
+        try: client.close()
+        except: pass     
+
+    now_playing = False
+
+
+class StarRailDiscordRPC:
+    def __init__(self, image):
+        self.status = False
+        text = "StarRailDiscordRPC.exe"
+        icon = Image.open(image)
+        menu = Menu(
+                    MenuItem(f"Version {VERSION}", self.open_website),
+                    MenuItem("終了", self.stop_program),
+                )
+        self.icon = Icon(name=text, title=text, icon=icon, menu=menu)
+
+    def run_schedule(self):
+        schedule.every(1).seconds.do(update_status)
+        while self.status:
+            schedule.run_pending()
+            time.sleep(1)
+
+    def run_program(self):
+        self.status = True
+        task_thread = threading.Thread(target=self.run_schedule)
+        task_thread.start()
+        self.icon.run()
+    
+    def stop_program(self):
+        self.status = False
+        self.icon.stop()
+
+    def open_website(self):
+        webbrowser.open(WEBSITE)
+
+
+if __name__ == "__main__":
+    system = StarRailDiscordRPC(image=resource_path("icon.ico"))
+    system.run_program()
