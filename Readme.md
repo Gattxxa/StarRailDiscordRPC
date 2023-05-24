6ec22769eb9385a152b9e83acef80a38be54e4af
Gattxxa <mail@gattxxa.org>
2023-05-24 14:13:24 +0900

    Support English named window

--- Readme.md
+++ Readme.md
@@ -1,6 +1,6 @@
 # <img src="https://download.gattxxa.org/StarRailDiscordRPC/favicon.ico" align="top" style="width: 1.4em"> StarRailDiscordRPC.exe  
 **[崩壊：スターレイル](https://hsr.hoyoverse.com/)を起動している間、DiscordのアクティビティにRich Presenceで表示させるやつ。**  
-仕様的に日本ユーザー向けです。
+Support to Japanese and English.
   
 ![](https://download.gattxxa.org/StarRailDiscordRPC/screenshot.png)
 
--- src/main.py
+++ src/main.py
@@ -11,13 +11,18 @@ import webbrowser
 import win32gui
 
 
-WINDOW_TITLE: str = "崩壊：スターレイル"
-CLIENT_ID: str = "1101576946086838365"
-VERSION: str = "1.2.0"
+WINDOW_TITLE_ENG: str = "Honkai: Star Rail"
+CLIENT_ID_ENG: str = "1110781234793160735"
+
+WINDOW_TITLE_JP: str = "崩壊：スターレイル"
+CLIENT_ID_JP: str = "1101576946086838365"
+
+VERSION: str = "1.2.1"
 WEBSITE: str = "https://download.gattxxa.org/StarRailDiscordRPC/"
 
 cooldown: int = 0
 now_playing: bool = False
+language: str = "en-us"
 status_updated: bool = False
 timestamp = time.mktime(time.localtime())
 client: rpc.WinDiscordIpcClient = rpc.DiscordIpcClient
@@ -32,18 +37,56 @@ def resource_path(relative_path):
     return os.path.join(os.path.abspath("."), relative_path)
 
 
-def find_window(hwnd, window_title):
-    global now_playing
-
-    if window_title in win32gui.GetWindowText(hwnd):
+def find_window(hwnd, _):
+    global now_playing, language
+    
+    if WINDOW_TITLE_ENG in win32gui.GetWindowText(hwnd):
         now_playing = True
+        language = "en-us"
         return hwnd
+    
+    if WINDOW_TITLE_JP in win32gui.GetWindowText(hwnd):
+        now_playing = True
+        language = "ja-jp"
+        return hwnd
+
+
+def setup_client(timestamp):
+    global client 
+
+    if language == "ja-jp":
+        window_title = WINDOW_TITLE_JP
+        client_id = CLIENT_ID_JP
+    else:
+        window_title = WINDOW_TITLE_ENG
+        client_id = CLIENT_ID_ENG
+
+    client = rpc.DiscordIpcClient.for_platform(client_id)
+    activity = {
+        "timestamps": {
+            "start": timestamp
+        },
+        "assets": {
+            "large_image": "application",
+            "large_text": f"{window_title}",
+        }
+    }
+
+    uid = ini.get('Profile', 'UID')
+    character = ini.get('Profile', 'Character')
+
+    if uid: 
+        activity["details"] = f"UID:{uid}"
+    if character:
+        activity["assets"]["small_image"] = f"{character}"
+
+    client.set_activity(activity)
 
 
 def update_status():
     global cooldown, timestamp, now_playing, status_updated, client
 
-    try: win32gui.EnumWindows(find_window, WINDOW_TITLE)
+    try: win32gui.EnumWindows(find_window, None)
     except: return
         
     if now_playing and status_updated:
@@ -56,27 +99,8 @@ def update_status():
             
         if cooldown < 1:
             cooldown = 300
-            client = rpc.DiscordIpcClient.for_platform(CLIENT_ID)
-            activity = {
-                "timestamps": {
-                    "start": timestamp
-                },
-                "assets": {
-                    "large_image": "application",
-                    "large_text": "崩壊：スターレイル",
-                }
-            }
-
-            uid = ini.get('Profile', 'UID')
-            character = ini.get('Profile', 'Character')
-
-            if uid: 
-                activity["details"] = f"UID:{uid}"
-            if character:
-                activity["assets"]["small_image"] = f"{character}"
-
-            client.set_activity(activity)
-                    
+            setup_client(timestamp)
+                
     elif status_updated:
         status_updated = False
         cooldown = 0
@@ -93,7 +117,7 @@ class StarRailDiscordRPC:
         icon = Image.open(image)
         menu = Menu(
                     MenuItem(f"Version {VERSION}", self.open_website),
-                    MenuItem("終了", self.stop_program),
+                    MenuItem("Exit", self.stop_program),
                 )
         self.icon = Icon(name=text, title=text, icon=icon, menu=menu)
 
